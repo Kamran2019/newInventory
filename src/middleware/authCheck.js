edfb592556ea60c57ba7e0bbb8337742f6b364a0
Kamran Ahmad <kamran1999ahmad@gmail.com>
2023-03-05 14:18:36 +0500

    session bug(3)

--- src/middleware/authCheck.js
+++ src/middleware/authCheck.js
@@ -1,6 +1,7 @@
 function checkAuthenticated(req, res, next) {
-  console.log(`checkAuthenticated: check authenticated`);
+  console.log(`Function check authenticated`);
   console.log(`session Id : ` + req.sessionID);
+  console.log(`about to check authenticated `);
   if (req.isAuthenticated()) {
     console.log(`checkAuthenticated: user is authenticated`);
     return next();
