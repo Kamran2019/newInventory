2f0bda83fd57a3774dfdaf26000cfc1be600cc80
Kamran2019 <kamrann1999ahmad@gmail.com>
2023-03-09 09:54:57 +0500

    multiple deserialize problem

--- app.js
+++ app.js
@@ -25,25 +25,16 @@ initializePassport(passport);
 var app = express();
 
 // view engine setup
+app.use(express.static(path.join(__dirname, "/src/public")));
 app.set("views", path.join(__dirname, "/src/views"));
 app.set("view engine", "ejs");
 
-//middleware
-// app.use(session({
-//   cookie:{
-//       secure: true,
-//       maxAge:60000
-//          },
-//   store: new RedisStore(),
-//   secret: 'secret',
-//   saveUninitialized: true,
-//   resave: false
-//   }));
 
 // Get Firestore Database from Admin SDK
 const db = admin.firestore();
 
 // Use FirestoreStore for Express session store
+//middleware
 const store = new FirestoreStore({
   database: db,
 });
@@ -63,7 +54,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/src/public")));
 app.use(flash());
 app.use(methodOverride("_method"));
 
